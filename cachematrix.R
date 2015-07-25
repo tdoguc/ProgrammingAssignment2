50787fb4f5100e3c6c9fb2fe6cceca7b0629c445
Taner <taner.doguc@finansbank.com.tr>
2015-07-25 12:15:39 +0300

    mynew version

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,40 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+
+makeCacheMatrix <- function(x = matrix()) {
+        # initialize the cachevalue as NULL
+        mycache <- NULL
+       
+        # store the input matrix
+        setMatrix <- function(newValue) 
+	  {
+                x <<- newValue
+                # since the matrix is assigned a new value, flush the cache
+                mycache <<- NULL
+        }
+ 
+        # returns the stored matrix
+        getMatrix <- function() 
+	  {
+                x
+        }
+ 
+        # cache the given argument
+        cacheInverse <- function(solve) 
+	  {
+                mycache <<- solve
+        }
+ 
+        # get the cached value
+        getInverse <- function() 
+	  {
+                mycache
+        }
+       
+        # returns a list where each element of the list is a function
+        list(setMatrix = setMatrix, getMatrix = getMatrix, cacheInverse = cacheInverse, getInverse = getInverse)
 }
 
 
